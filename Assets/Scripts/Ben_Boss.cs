e6cf121b5815a6ce22e57675df154893444dd114
Ben <benjamin.har6874@mediadesign.school.nz>
2018-02-28 09:38:19 +1300

    Added an AI for the boss and changed movement so that the lesser AI waits at it's destination for a random time

--- Assets/Scripts/Ben_Boss.cs
+++ Assets/Scripts/Ben_Boss.cs
@@ -3,24 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Ben_AI : MonoBehaviour
+public class Ben_Boss : MonoBehaviour
 {
     // Use this for initialization
     protected NavMeshAgent nav;               // Reference to the nav mesh agent.
-    protected bool Aggro = false;
     protected bool Combat = false;
     public Transform[] points;
     protected int destPoint = 0;
     protected float WaitTimer = 0.0f;
     protected float CombatTimer = 0.0f;
+    protected float ChargeTimer = 0.0f;
     public Transform target;
     protected Vector3 ChargeDestinationVector;
     protected Vector3 targetDir;
     protected float angle;
-    protected float RandomWait;
+
     void Awake()
     {
-        RandomWait = Random.Range(15.0f, 25.0f);
         nav = GetComponent<NavMeshAgent>();
     }
 
@@ -35,30 +34,15 @@ public class Ben_AI : MonoBehaviour
         angle = Vector3.Angle(targetDir, this.gameObject.transform.forward);
         WaitTimer += Time.deltaTime;
         CombatTimer += Time.deltaTime;
-        if (angle < 1.0f)
-        {
-            Debug.Log("Called Aggro");
-            Aggro = true;
-        }
-        else
-        {
-            Aggro = false;
-        }
-
-        if (targetDir.x < 10.0f && targetDir.z < 10.0f && Aggro)
+        if (angle < 10.0f)
         {            
+            Debug.Log("Called Combat");
             Combat = true;
         }
 
         if (Combat && (CombatTimer > 3.0f))
         {
-            CombatTimer = 0.0f;
-            //nav.speed += 0.5f;
-            nav.SetDestination(target.position);
-        }
-        else if (Aggro)
-        {
-            nav.SetDestination(target.position);
+            Charge(target.position);
         }
         else
         {
@@ -68,9 +52,13 @@ public class Ben_AI : MonoBehaviour
                 {
                     nav.isStopped = true;
                 }
+                float RandomWait = Random.Range(5.0f, 10.0f);
                 if (WaitTimer >= RandomWait)
                 {
+                    nav.speed = 4.0f;
+                    nav.isStopped = false;
                     GotoNextPoint();
+                    WaitTimer = 0.0f;
                 }
             }
         }
@@ -80,9 +68,8 @@ public class Ben_AI : MonoBehaviour
     {
         if (_collision.gameObject.tag == "Player")
         {
-          //end game here? or just damage player
+            //end game here
         }
-
         if (_collision.gameObject.tag == "BossWall")
         {
             CombatTimer = 0.0f;
@@ -92,8 +79,6 @@ public class Ben_AI : MonoBehaviour
 
     public void GotoNextPoint()
     {
-        nav.isStopped = false;
-        WaitTimer = 0.0f;
         // Returns if no points have been set up
         if (points.Length == 0)
         {
@@ -105,5 +90,18 @@ public class Ben_AI : MonoBehaviour
         // Choose the next point in the array as the destination,
         // cycling to the start if necessary.
         destPoint = (destPoint + 1) % points.Length;
+    }
+    void Charge(Vector3 _ChargeLoc)
+    {
+        ChargeTimer += Time.deltaTime;
+        Debug.Log("CHARGE");
+        nav.speed = 16.0f;
+        ChargeDestinationVector = (target.position - this.gameObject.transform.position);
+        ChargeDestinationVector = ChargeDestinationVector.normalized * 500.0f;
+        nav.SetDestination(ChargeDestinationVector);
+        if (ChargeTimer > 1.0f)
+        {
+            ChargeDestinationVector = this.gameObject.transform.position;
+        }
     }
 }
