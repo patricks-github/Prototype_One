c85b16c8e4b6eb4abb0b5bcf42b2e00de64bdcb2
PARTICK\Partick <patrickmansonwork@gmail.com>
2018-03-06 21:28:40 +1300

    Added new Roller model and textures, made a Boss_Animated prefab using the animators, need some work on the animations for better transitions but should work now.  Attacks can be triggered using the public void in the Boss movement script.

---
+++ Assets/Boss_Movement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Boss_Movement : MonoBehaviour {
+
+    Animator BossAnimator;
+    NavMeshAgent navMeshAgent;
+    public float speedDisplayModifier = 1.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        BossAnimator = gameObject.GetComponent<Animator>();
+        navMeshAgent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BossAnimator.SetFloat("speed", navMeshAgent.velocity.magnitude * speedDisplayModifier);
+    }
+
+    public void AttackAnimation()
+    {
+        BossAnimator.SetTrigger("attack");
+    }
+}
